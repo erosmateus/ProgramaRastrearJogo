cf8370945904a137a475e0ad34f9750cdb5bc912
Eros Mateus Zapchau <erosmateus2003@gmail.com>
2024-03-27 20:34:15 -0300

    Programa para selecionar o jogo desejado

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ProgramaPesquisaJogo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.FloralWhite;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2K",
+            "Rockstar",
+            "Naughty Dog",
+            "Santa Monica",
+            "Team Cherry",
+            "Super Giant"});
+            this.comboBox1.Location = new System.Drawing.Point(150, 102);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.FloralWhite;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(150, 222);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "EMPRESA:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "JOGO:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSalmon;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(355, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(385, 358);
+            this.panel1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(15, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(355, 324);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Visualizador de jogos";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramaPesquisaJogo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] nomeImagensJogos = Directory.GetFiles(@"C:\Users\erosm\source\repos\ProgramaPesquisaJogo\ImagensJogos\Jogos");
+
+            foreach (string img in nomeImagensJogos) 
+            {
+                string[] separador = img.Split('\\');
+                string[] nomeSelecionado = separador[8].Split('.');
+                
+                if(comboBox2.Text == nomeSelecionado[0])
+                {
+                    pictureBox1.ImageLocation = @"C:\Users\erosm\source\repos\ProgramaPesquisaJogo\ImagensJogos\Jogos\" + comboBox2.Text + ".jpg";
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string jogos = comboBox1.Text;
+            comboBox2.Text = string.Empty;
+
+            switch (jogos)
+            {
+                case "2K":
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.Add("2K24");
+                    comboBox2.Items.Add("Bioshock");
+                    comboBox2.Items.Add("Borderlands");
+                    break;
+
+                case "Rockstar":
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.Add("Bully");
+                    comboBox2.Items.Add("GTAV");
+                    comboBox2.Items.Add("Read Dead Redemption 2");
+                    break;
+
+                case "Naughty Dog":
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.Add("Crash Bandicoot");
+                    comboBox2.Items.Add("The last of Us");
+                    comboBox2.Items.Add("Uncharted");
+                    break;
+
+                case "Santa Monica":
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.Add("God of War 2018");
+                    comboBox2.Items.Add("God of War Ragnarok");
+                    break;
+
+                case "Team Cherry":
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.Add("Hollow Knight");
+                    comboBox2.Items.Add("Hollow Knight Silksong");
+                    break;
+
+                case "Super Giant":
+                    comboBox2.Items.Clear();
+                    comboBox2.Items.Add("Hades");
+                    comboBox2.Items.Add("Hades 2");
+                    break;
+            }
+        }
+    }
+}
